3bbaf0463e5cb14deece723804842b524c88c0d2
kylebrain <kbrainard@nevada.unr.edu>
2020-02-11 17:45:00 -0800

    Fixed bug added to main

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tictactoe.game import Game
 from tictactoe.player import HumanPlayer, MockPlayer
 
 def main():
-    game = Game(HumanPlayer('X'), MockPlayer('O'))
+    game = Game(HumanPlayer('X'), HumanPlayer('O'))
     # game = Game(HumanPlayer('X'), MockPlayer('O', [(0,0), (1,1), (2,2)]))
     game.play_game()
 
